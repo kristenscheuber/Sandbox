2d15f1c85d644853669a6cee46968ab2cfbe60c3
kristenscheuber <kristen.scheuber@my.jcu.edu.au>
2018-03-09 20:16:24 +1000

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,11 @@
-"""Kristen Scheuber"""
+"""
+Kristen Scheuber
+
+Ask the user for their name, error-check to make sure their name is not blank then print every second letter
+of their name.
+"""
+
+user_name = input("Your Name: ")
+while user_name == "":
+    user_name = input("Your Name: ")
+print(user_name[::2])
